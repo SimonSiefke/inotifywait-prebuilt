fd7eb19b58e6e421f5dbee46d2fba0def73c2953
Simon Siefke <simon.siefke@gmail.com>
2021-09-25 15:48:15 +0200

    try to fix double tar gz

--- 04_package.sh
+++ 04_package.sh
@@ -3,7 +3,6 @@
 set -e
 
 OUT_DIR="release"
-
 rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
-tar czvf "$OUT_DIR/inotifywait.tar.gz" ./inotify-tools/src/inotifywait
+cp ./inotify-tools/src/inotifywait "$OUT_DIR/"
